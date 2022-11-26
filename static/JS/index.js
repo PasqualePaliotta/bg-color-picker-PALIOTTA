0898e1416462b35d592e1813d05b72e560e2ecb4
PasqualePaliotta <114742509+PasqualePaliotta@users.noreply.github.com>
2022-11-26 11:47:41 +0100

    js creato

---
+++ static/JS/index.js
@@ -0,0 +1,24 @@
+
+function cambio1() {
+    document.body.style.background = "#FE0202";
+}
+
+function cambio2() {
+    document.body.style.background = "#59BC98";
+}
+
+function cambio3() {
+    document.body.style.background = "#2594D2";
+}
+
+function cambio4() {
+    document.body.style.background = "#72BC59";
+}
+
+function cambio5() {
+    document.body.style.background = "#BC59A0";
+}
+
+function cambio6() {
+    document.body.style.background = "#DCF73C";
+}
